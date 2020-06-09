397d68c5b3fadbd4f83688a183b17646f4bf2b01
Johny W. Alves <johnywalves@gmail.com>
2020-06-09 19:59:09 -0300

    :package: Add gatsby-plugin-styled-components

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,2 +1,3 @@
-import "lazysizes"
-require("prismjs/themes/prism-tomorrow.css")
+import "lazysizes";
+
+require("prismjs/themes/prism-tomorrow.css");
--- gatsby-config.js
+++ gatsby-config.js
@@ -13,6 +13,7 @@ module.exports = {
     plugins: [
         `gatsby-plugin-react-helmet`,
         `gatsby-plugin-transition-link`,
+        `gatsby-plugin-styled-components`,
         {
             resolve: `gatsby-plugin-root-import`,
             options: {
