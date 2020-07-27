3e1d92a19712c8e6d0fae9a0cac335846c240d01
Johny W. Alves <johnywalves@gmail.com>
2020-07-27 20:37:11 -0300

    :fire: Remover Material UI

--- src/assets/jss/material-kit-react/views/vitaePage.jsx
+++
@@ -1,7 +0,0 @@
-const vitaePageStyle = {
-  verticalTimelineWork: {
-    boxShadow: '0 2px 2px 0 rgba(100, 100, 100, 0.14), 0 3px 1px -2px rgba(100, 100, 100, 0.2), 0 1px 5px 0 rgba(100, 100, 100, 0.12)'
-  }
-};
-
-export default vitaePageStyle;
