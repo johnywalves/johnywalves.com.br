366320667a67c9eb4219c88486527d0601a102d0
Johny W. Alves <johnywalves@gmail.com>
2020-08-20 22:01:06 -0300

    :sparkles: Add validação para atualização

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,3 +1,14 @@
 import "lazysizes"
+import React from "react"
+import ReactDOM from "react-dom"
+import SWUpdater from "./src/components/SWUpdater"
 
 require("prismjs/themes/prism-tomorrow.css")
+
+function onServiceWorkerUpdateReady() {
+    const root = document.createElement('div')
+    document.body.appendChild(root);
+    ReactDOM.render(<SWUpdater></SWUpdater>, root)
+}
+
+export { onServiceWorkerUpdateReady }
