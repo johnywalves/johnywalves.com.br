8adfee2f9d58e25e42d1d9ce17910bdf573d19fb
Johny W. Alves <johnywalves@gmail.com>
2020-08-20 22:09:48 -0300

    :dizzy: Smooth scroll para links para id

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,6 +5,13 @@ import SWUpdater from "./src/components/SWUpdater"
 
 require("prismjs/themes/prism-tomorrow.css")
 
+if (typeof window !== 'undefined') {
+    require('smooth-scroll')('a[href*="#"]', {
+        speed: 200,
+        offset: 66
+    })
+}
+
 function onServiceWorkerUpdateReady() {
     const root = document.createElement('div')
     document.body.appendChild(root);
