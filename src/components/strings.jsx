bb354ceb4bd4f909ece98cfe0023c4258f5da1aa
Johny W. Alves <johnywalves@gmail.com>
2022-10-27 13:25:07 -0300

    :recycle: Padronização de exportações

--- src/components/strings.jsx
+++ src/components/strings.jsx
@@ -3,7 +3,9 @@ import LocalizedStrings from "react-localization"
 import brazilian from "assets/lang/br.json"
 import english from "assets/lang/en.json"
 
-export default new LocalizedStrings({
+const Strings = new LocalizedStrings({
   pt: brazilian,
   en: english,
 })
+
+export default Strings
